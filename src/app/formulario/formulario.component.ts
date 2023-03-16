b62067993c6b9ae252c16a3a8446f1b46da7e9d8
UrojasG <urielelihurojasgarfias@gmail.com>
2023-03-16 15:36:30 -0600

    feat: local references

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -8,11 +8,11 @@ import { Person } from '../person.model';
 })
 export class FormularioComponent {
   @Output() personBuild = new EventEmitter<Person>();
-  nameInput: string = '';
-  Last_nameInput: string = '';
+  //nameInput: string = '';
+  //Last_nameInput: string = '';
 
-  addPerson(){
-    let person1 = new Person(this.nameInput, this.Last_nameInput);
+  addPerson(nameInput: HTMLInputElement, Last_nameInput: HTMLInputElement){
+    let person1 = new Person(nameInput.value,Last_nameInput.value);
     //this.persons.push(person1); //this way show data in the same component
     this.personBuild.emit(person1); //thso way show data in the father component
   }
